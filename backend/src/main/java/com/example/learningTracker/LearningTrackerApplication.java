ecb0c9ec0550332c645b0b79efac21db58a5a509
michida2110545 <stupid763@gmail.com>
2024-12-12 18:15:22 +0900

    Initial commit

---
+++ backend/src/main/java/com/example/learningTracker/LearningTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTrackerApplication.class, args);
+	}
+
+}
