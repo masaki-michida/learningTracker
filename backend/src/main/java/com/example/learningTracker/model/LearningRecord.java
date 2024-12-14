594211ce678bfc053461b258794fb1dba567b04b
admin <admin@adminnoMacBook-Air.local>
2024-12-14 10:28:21 +0900

    backend

--- backend/src/main/java/com/example/learningTracker/model/LearningRecord.java
+++ backend/src/main/java/com/example/learningTracker/model/LearningRecord.java
@@ -1,13 +1,26 @@
+// backend/src/main/java/com/example/learningTracker/model/LearningRecord.java
+
 package com.example.learningTracker.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
 import java.time.LocalDateTime;
 
+@Entity//データベーステーブルになることを示す
+@Getter
+@Setter//lombokのアノテーションで、getter, setter, toString, equals, hashCodeメソッドを自動生成
 public class LearningRecord {
-    private Long id;
+    @Id//主キー
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//ID生成方法を指定今回は自動生成
+    private Integer id;
+    
     private String title;
     private String content;
+    private Integer studyMinutes;
     private LocalDateTime createdAt;
-
-    // getters and setters
-    // コンストラクタ
-} 
+    
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
